d5f537f0192645a5b077d78745dd53e883879309
James Black <james@james-black.com>
2013-12-18 19:28:19 -0800

    Adding the gameloop

--- game.py
+++ game.py
@@ -1,25 +1,43 @@
+#Import core libs
+import os
+from core.libs import *
+
 #Import all the items
 from items import *
 from items.weapons import *
+
+
 
 base_weapon = BaseWeapon()
 sword = Sword()
 dagger = Dagger()
 
-print "Base Weapon"
-print "======================="
-print base_weapon.name
-print base_weapon.hit_chance
-print base_weapon.damage
+running = True
 
-print "Sword Weapon"
-print "======================="
-print sword.name
-print sword.hit_chance
-print sword.damage
+while (running):
 
-print "Dagger Weapon"
-print "======================="
-print dagger.name
-print dagger.hit_chance
-print dagger.damage
+  os.system("clear")
+
+  print "Base Weapon"
+  print "======================="
+  print base_weapon.name
+  print base_weapon.hit_chance
+  print base_weapon.damage
+
+  print "Sword Weapon"
+  print "======================="
+  print sword.name
+  print sword.hit_chance
+  print sword.damage
+
+  print "Dagger Weapon"
+  print "======================="
+  print dagger.name
+  print dagger.hit_chance
+  print dagger.damage
+
+  action = getch()
+
+  if action.lower() == "q":
+    os.system("clear")
+    running = False
